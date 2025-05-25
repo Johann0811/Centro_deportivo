4fb41b1fe4140b66b0099d4f53ecd0800cee4641
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-25 18:59:27 -0500

    Creo la interfaz del programa con tkinter ya nombrando las variables de los botones para tener una estructura en la que basarme al momento de empezar a desarrollar la interfaz

---
+++ interfaz.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from tkinter.scrolledtext import ScrolledText
+from tkinter import messagebox
+from procesos import archivo
+from procesos import rendimiento
+from procesos import DataAnalyzer
+from procesos import PuntajeAnalyzer
+
+ventana = tk.Tk()
+ventana.title("Reporte de estadisticas del centro deportivo")
+ventana.geometry()
+
+boton_registro = tk.Button(ventana)
+boton_registro.grid(row=0, column=0)
+
+boton_reporte_general = tk.Button(ventana)
+boton_reporte_general.grid(row=0, column=1)
+
+boton_reporte_individual = tk.Button(ventana)
+boton_reporte_individual.grid(row=0, column=2)
+
+boton_salir = tk.Button(ventana)
+boton_salir.grid(row=1, column=0)
+
+text_area = ScrolledText(ventana, width= 70, height= 30)
+text_area.grid(row=1, column=1)
+
+content_frame = tk.Frame(ventana)
+content_frame.grid(row=1, column=2)
+
+
+ventana.mainloop()

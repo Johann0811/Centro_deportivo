5319092ecc0a9bbde753b946870d385cfa37dfc6
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-25 17:38:02 -0500

    Cree un diccionario vacio el cual tiene de claves todos los datos que voy a necesitar despues, se creo la clase rendimiento la cual contiene un subproceso llamado registro donde se añade al participante, su puntuacion, se crea el nivel de dificultad para cada prueba y se hace el calculo de su puntaje final con la formula dada, ademas si todavia no existe el archivo donde se almacenan los datos este se creara y al final se añaden como valores los puntajes y dificultades obtenidas a sus respectivas claves

---
+++ procesos.py
@@ -0,0 +1,72 @@
+import random as rd
+import pandas as pd
+import tkinter as tk
+from tkinter import simpledialog, messagebox
+import os
+import csv
+import matplotlib.pyplot as plt
+import seaborn as sns 
+archivo = "deportes.csv"
+df = None
+
+registro = {"Nombre": None, "Puntaje_total": None, "Clasificacion": None,
+            "Puntaje_resistencia": None, "Puntaje_fuerza": None, "Puntaje_velocidad": None,
+            "dificultad_resistencia": None, "dificultad_fuerza": None, "dificultad_velocidad": None,
+            "Puntaje_resistencia_final": None, "Puntaje_fuerza_final": None, "Puntaje_velocidad_final": None}
+
+class rendimiento():
+    def registro(self):
+        registro["Nombre"] = simpledialog.askstring("Registro","Ingrese el nombre del participantes")
+        while True:   
+            puntaje1 = simpledialog.askinteger("Puntajes. Recuerde que se califica de 0-100", "Cuanto puntaje obtuvó en la prueba de resistencia?: ")
+            if puntaje1 < 0 or puntaje1 > 100:
+                messagebox.showerror("ERROR", "El puntaje solo puede ser de 0-100")
+                continue 
+            else:
+                dificultad1 = round(rd.uniform(1.0,1.3),2)
+                registro["Puntaje_resistencia"] = puntaje1
+                registro["dificultad_resistencia"] = dificultad1
+                calculo_puntaje1 = round(puntaje1 * dificultad1)
+                registro["Puntaje_resistencia_final"] = calculo_puntaje1 
+                break 
+        
+        while True:   
+            puntaje2 = simpledialog.askinteger("Puntajes. Recuerde que se califica de 0-100", "Cuanto puntaje obtuvó en la prueba de fuerza?: ")
+            if puntaje2 < 0 or puntaje2 > 100:
+                messagebox.showerror("ERROR", "El puntaje solo puede ser de 0-100")
+                continue 
+            else:                        
+                dificultad2 = round(rd.uniform(1.0,1.3),2)
+                registro["Puntaje_fuerza"] = puntaje2
+                registro["dificultad_fuerza"] = dificultad2
+                calculo_puntaje2 = round(puntaje2 * dificultad2)
+                registro["Puntaje_fuerza_final"] = calculo_puntaje2
+                break
+                
+        while True:   
+            puntaje3 = simpledialog.askinteger("Puntajes. Recuerde que se califica de 0-100", "Cuanto puntaje obtuvó en la prueba de velocidad?: ")
+            if puntaje3 < 0 or puntaje3 > 100:
+                messagebox.showerror("ERROR", "El puntaje solo puede ser de 0-100")
+                continue 
+            else:                        
+                dificultad3 = round(rd.uniform(1.0,1.3),2)
+                registro["Puntaje_velocidad"] = puntaje3
+                registro["dificultad_velocidad"] = dificultad3
+                calculo_puntaje3 = round(puntaje3 * dificultad3)
+                registro["Puntaje_velocidad_final"] = calculo_puntaje3 
+                break
+            
+    
+        puntaje_final = round(sum([calculo_puntaje1,calculo_puntaje2,calculo_puntaje3])/sum([dificultad1,dificultad2,dificultad3]))
+        if puntaje_final >= 70:
+            registro["Clasificacion"] = "Clasifico"
+        else:
+            registro["Clasificacion"] = "No clasifico"
+        
+        registro["Puntaje_total"] = puntaje_final
+        archivo_existe = os.path.isfile('deportes.csv')
+        with open(archivo, 'a', newline='') as f:
+            escritor = csv.DictWriter(f, fieldnames=registro.keys())
+            if not archivo_existe:
+                escritor.writeheader()
+            escritor.writerow(registro)

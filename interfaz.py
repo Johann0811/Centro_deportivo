06e487c6562999344eed2e0fefd5beff49cfa312
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-25 19:13:54 -0500

    agrege la variable reporte que muestra el reporte global y ademas cree otro text_area para que se puedan visualizar los datos de los participantes y su promedio y resumen estadistico al mismo tiempo

--- interfaz.py
+++ interfaz.py
@@ -18,6 +18,26 @@ def registro():
      else:
         df = pd.read_csv(archivo)
 
+def reporte():
+    archivo_existe = os.path.isfile('deportes.csv')
+    if not archivo_existe:
+        messagebox.showwarning("Error", "No existen datos")
+    else:
+        df = pd.read_csv(archivo)
+        Rendimiento = rendimiento()
+        reporte = Rendimiento.reporte_general()
+        text_area.delete("1.0",tk.END)
+        text_area.insert(tk.END, reporte.to_string(index=True))
+        pruebas = PuntajeAnalyzer(df)
+        promedio_grupo = pruebas.promedio()
+        descripcion = round(pruebas.describe())
+        text_area2.delete("1.0",tk.END)
+        text_area2.insert(tk.END, "Promedio del grupo\n")
+        text_area2.insert(tk.END,f"{promedio_grupo}\n\n")
+        text_area2.insert(tk.END, "Resumen estadistico\n")
+        text_area2.insert(tk.END,descripcion)
+        pruebas.estadistica_grupal()
+
 ventana = tk.Tk()
 ventana.title("Reporte de estadisticas del centro deportivo")
 ventana.geometry()
@@ -25,7 +45,7 @@ ventana.geometry()
 boton_registro = tk.Button(ventana, text="Registrar participante", command= registro)
 boton_registro.grid(row=0, column=0)
 
-boton_reporte_general = tk.Button(ventana)
+boton_reporte_general = tk.Button(ventana, text="Mostrar reporte general", command=reporte)
 boton_reporte_general.grid(row=0, column=1)
 
 boton_reporte_individual = tk.Button(ventana)
@@ -36,6 +56,8 @@ boton_salir.grid(row=1, column=0)
 
 text_area = ScrolledText(ventana, width= 70, height= 30)
 text_area.grid(row=1, column=1)
+text_area2 = ScrolledText(ventana, width= 100, height= 30)
+text_area2.grid(row=1, column=2)
 
 content_frame = tk.Frame(ventana)
 content_frame.grid(row=1, column=2)

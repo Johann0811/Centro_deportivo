f95f4e357c04ce62a1f6023f2cbf2d9a14492e41
Johann0811 <johann_edu.gonzalez@uao.edu.co>
2025-05-25 19:17:30 -0500

    Se agrego la variable reporte_individual la cual muestra el reporte de la persona deseada y sus histogramas ademas de añadirse el boton para salir de la ventana y cerrarla tambien se creo un tercer text_area para poder visualizar todos los datos al mismo tiempo sin tener que sobreescribir en alguno al momento de visualizar o hacerlo desaparecer

--- interfaz.py
+++ interfaz.py
@@ -38,6 +38,21 @@ def reporte():
         text_area2.insert(tk.END,descripcion)
         pruebas.estadistica_grupal()
 
+def reporte_individual():
+    archivo_existe = os.path.isfile('deportes.csv')
+    if not archivo_existe:
+        messagebox.showwarning("Error", "No existen datos")
+    else:
+        df = pd.read_csv(archivo)
+        Rendimiento = rendimiento() 
+        text_area3.delete("1.0",tk.END)
+        reporte_solo = Rendimiento.reporte_individual()
+        text_area3.insert(tk.END,reporte_solo)
+
+def terminar():
+    ventana.destroy()
+    #https://www.geeksforgeeks.org/destroy-method-in-tkinter-python/  
+
 ventana = tk.Tk()
 ventana.title("Reporte de estadisticas del centro deportivo")
 ventana.geometry()
@@ -48,16 +63,18 @@ boton_registro.grid(row=0, column=0)
 boton_reporte_general = tk.Button(ventana, text="Mostrar reporte general", command=reporte)
 boton_reporte_general.grid(row=0, column=1)
 
-boton_reporte_individual = tk.Button(ventana)
+boton_reporte_individual = tk.Button(ventana, text="Buscar participante", command= reporte_individual)
 boton_reporte_individual.grid(row=0, column=2)
 
-boton_salir = tk.Button(ventana)
+boton_salir = tk.Button(ventana, text="Salir", command=terminar)
 boton_salir.grid(row=1, column=0)
 
 text_area = ScrolledText(ventana, width= 70, height= 30)
 text_area.grid(row=1, column=1)
 text_area2 = ScrolledText(ventana, width= 100, height= 30)
 text_area2.grid(row=1, column=2)
+text_area3 = ScrolledText(ventana, width=70, height= 10)
+text_area3.grid(row=2, column=1)
 
 content_frame = tk.Frame(ventana)
 content_frame.grid(row=1, column=2)
